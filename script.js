6c4e134ca0eb8d0b60b35638528e07c7756a8a1d
Dharan13 <160810270+Dharan13@users.noreply.github.com>
2024-03-11 01:45:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+//Question No.2 Resume in JSON Format
+let resume={
+    "Basics Information":{
+        "Name":"dharanidhran",
+    "EmailID":"dharansdharan36@gmail.com",
+    "MobileNo":"9156783974",
+    "Address":"alandur, chennai"},
+    
+    "Graduation Details" :{ 
+
+    "UG":{
+      "Institute":"meenakshi college of enginering",
+      "course":"BE Mechanical engineering",
+      "Year of Passing":"2019",
+      "CGPA":"6.2"},
+
+    "HSC":{
+        "Institute":"AKT Matriculation school",
+        "Year of Passing":"2015",
+        "Percentage":"65%"},
+
+    "SSLC":{
+        "Institute":"Oxford Matriculation school",
+        "Year of Passing":"2013",
+        "Percentage":"91.6%"}},
+
+    "Skills":{
+            "Software kmown " : "microsoft",
+            "Speaking language":"Tamil and English"}
+             
+    }
+    
+    {
+    console.log(resume);
+    }
